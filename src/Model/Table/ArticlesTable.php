67c2993be875b2052c36a7b34b9123503d2bcfaf
Marcelo Rocha <contato@omarcelo.com.br>
2020-05-25 10:06:47 -0300

    Strict cs checks

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Copyright 2020, Cake Development Corporation (https://www.cakedc.com)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright 2020, Cake Development Corporation (https://www.cakedc.com)
- *  @license MIT License (http://www.opensource.org/licenses/mit-license.php)
- */
-
 declare(strict_types=1);
 
 namespace App\Model\Table;
@@ -71,8 +61,7 @@ class ArticlesTable extends Table
     }
 
     /**
-     * @param \Cake\Datasource\EntityInterface $article
-     *
+     * @param \Cake\Datasource\EntityInterface $article The article to clear.
      * @return bool
      */
     public function clearArticle($article)
